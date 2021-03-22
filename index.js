fb6aa5145bfae398410da5735611a3d177d2e06e
qinghuan-deep <1245816264@qq.com>
2021-03-22 10:52:03 +0800

    add gitignore

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-const { ApiPromise, WsProvider } = require('@polkadot/api');
-const { encodeAddress } = require('@polkadot/keyring');
-const { options } = require('@chainx-v2/api');
+const {
+    ApiPromise,
+    WsProvider
+} = require('@polkadot/api');
+const {
+    encodeAddress
+} = require('@polkadot/keyring');
+const {
+    options
+} = require('@chainx-v2/api');
 const url = 'wss://chainx.elara.patract.io';
 const wsProvider = new WsProvider(url);
-const { Keyring } = require('@polkadot/keyring')
-const { cryptoWaitReady, mnemonicGenerate }=  require('@polkadot/util-crypto');
-
+const {
+    Keyring
+} = require('@polkadot/keyring')
+const {
+    cryptoWaitReady,
+    mnemonicGenerate
+} = require('@polkadot/util-crypto');
 
 
 const mnemonic = mnemonicGenerate();
 // eslint-disable-next-line no-void
 async function excuteTransfer() {
-    
-    const api = await ApiPromise.create(options({ provider: wsProvider }));
+
+    const api = await ApiPromise.create(options({
+        provider: wsProvider
+    }));
 
     await cryptoWaitReady();
 
     const systemProperties = await api.rpc.system.properties()
     properties = systemProperties.toJSON()
 
-    const keyring = new Keyring({ type: 'sr25519', ss58Format: properties.ss58Format })
-    
+    const keyring = new Keyring({
+        type: 'sr25519',
+        ss58Format: properties.ss58Format
+    })
+
     // create an sr25519 pair from the mnemonic (keyring defaults)
-    const sp = keyring.createFromUri('0xb79017552da1b79c630fb2ed82d111350f6157a75d3076a2d4c4adfb0ed4d605', { name: 'test-account' });
-   
+    const sp = keyring.createFromUri('0xb79017552da1b79c630fb2ed82d111350f6157a75d3076a2d4c4adfb0ed4d605', {
+        name: 'test-account'
+    });
+
     console.log('address:' + sp.address);
 
     const balance = await api.query.system.account(sp.publicKey);
@@ -36,9 +54,9 @@ async function excuteTransfer() {
     // Sign and send the transaction using our account
     const hash = await transfer.signAndSend(sp);
 
-   console.log('Transfer sent with hash', hash.toHex());
+    console.log('Transfer sent with hash', hash.toHex());
 
-  process.exit();
+    process.exit();
 }
 
 async function main() {
